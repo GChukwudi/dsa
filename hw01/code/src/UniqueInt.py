b8a57d025c03e997d865455b0d44ad0c6068d9d6
God'sFavour Chukwudi <127259967+GChukwudi@users.noreply.github.com>
2024-05-12 11:13:56 +0200

    updated

--- hw01/code/src/UniqueInt.py
+++ hw01/code/src/UniqueInt.py
@@ -26,33 +26,20 @@ class UniqueInt:
 
         end_time = time.time()
         return end_time - start_time
-# import sys
 
-# class UniqueInt:
-#     @staticmethod
-#     def processFile(inputFilePath, outputFilePath):
-#         unique_integers = set()
-#         with open(inputFilePath, 'r') as inputFile:
-#             for line in inputFile:
-#                 line = line.strip()
-#                 if line:
-#                     try:
-#                         num = int(line)
-#                         if -1023 <= num <= 1023:
-#                             unique_integers.add(num)
-#                     except ValueError:
-#                         pass
+    
+if __name__ = "__main__":
+    current_dir = os.path.dirname(os.path.abspath(__file__))
 
-#         with open(outputFilePath, 'w') as outputFile:
-#             for num in sorted(unique_integers):
-#                 outputFile.write(str(num) + '\n')
+    input_dir = os.path.join(current_dir, "../../sample_inputs")
+    output_dir = os.path.join(current_dir, "../../sample_results")
 
-# if __name__ == "__main__":
-#     input_dir = "/dsa/hw01/sample_inputs"
-#     output_dir = "/dsa/hw01/sample_outputs"
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
-#     for filename in os.listdir(input_dir):
-#         if filename.endswith(".txt"):
-#             inputFilePath = os.path.join(input_dir, filename)
-#             outputFilePath = os.path.join(output_dir, filename.replace(".txt", "_output.txt"))
-#             UniqueInt.processFile(inputFilePath, outputFilePath)
+    for filename in os.listdir(input_dir):
+        if filename.endswith(".txt"):
+            inputFilePath = os.path.join(input_dir, filename)
+            outputFilePath = os.path.join(output_dir, filename.replace(".txt", "_result.txt"))
+            runtime = UniqueInt.processFile(inputFilePath, outputFilePath)
+            print(f"Processed {filename} in {runtime:.2f} seconds.")
